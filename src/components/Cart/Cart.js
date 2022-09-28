126c65d4d7df262a0449d1d8e0cb60ec08ff53d5
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2022-09-28 12:04:41 +0600

    calculation completed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
+    console.log(cart)
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        total = total + product.price* product.quantity;
+        shipping = shipping + product.shipping;
+        quantity = quantity + product.quantity;
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
     return (
         <div className='cart'>
             <h3>Orders summary</h3>
-                <h3>Selected Items: {cart.length}</h3>
+            <p>Selected Items: ${quantity}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal}</h5>
         </div>
     );
 };
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,17 +4,19 @@ import React from 'react';
 import './Product.css'
 
 const Product = ({ handleAddToCart, product }) => {
-    const { img,name,price,ratings,seller}=product;
+    const { img,name,price,ratings,seller,shipping}=product;
     return (
         <div className='product'>
             <img src={img} alt="" />
            <div className='product-info'>
            <p className='product-name'>{name}</p>
             <p>Price: ${ price}</p>
-            <p>Manufacturer: { seller}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Shipping: { shipping}</p>
             <p>Rating: { ratings} star</p>
            </div>
-            <button onClick={()=>handleAddToCart(product)} className='btn-cart'>
+            <button onClick={() => handleAddToCart(product)} 
+            className='btn-cart'>
                 <p className='btn-text'>Add To Cart</p>
                 <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
